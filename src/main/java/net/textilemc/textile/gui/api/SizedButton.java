7df1e7f8df5fd98a1b25115fbc656a591813939c
sschr15 <steven.sschr15@hotmail.com>
2020-11-27 15:11:13 -0600

    Sized Button

---
+++ src/main/java/net/textilemc/textile/gui/api/SizedButton.java
@@ -0,0 +1,12 @@
+package net.textilemc.textile.gui.api;
+
+import net.minecraft.client.gui.Button;
+
+/**
+ * A button that can be any size.
+ */
+public class SizedButton extends Button {
+    public SizedButton(int i, int i1, int i2, int width, int height, String text) {
+        super(i, i1, i2, width, height, text);
+    }
+}
