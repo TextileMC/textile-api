e837eea4a86ec8f112e47dafa550bf97671aec16
sschr15 <steven.sschr15@hotmail.com>
2020-11-27 14:53:49 -0600

    Change to API details, add basic GUI stuff

---
+++ src/main/java/net/textilemc/textile/gui/mixin/ButtonMixin.java
@@ -0,0 +1,20 @@
+package net.textilemc.textile.gui.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.Button;
+import net.minecraft.client.gui.DrawableHelper;
+
+@Mixin(Button.class)
+public abstract class ButtonMixin extends DrawableHelper {
+    @Shadow private int field_939;
+
+    @Inject(method = "<init>(IIIIILjava/lang/String;)V", at = @At("RETURN"))
+    private void onInit(int i, int i2, int i3, int width, int height, String text, CallbackInfo ci) {
+        this.field_939 = height;
+    }
+}
