fb74ed2887234c150061f5b6e144463d5dceb171
SD <harrypotter.starwars.shrish@gmail.com>
2020-11-29 07:31:08 +0530

    Tristate extensions

--- textile-api-base/src/main/java/net/textilemc/textile/api/util/BooleanFunction.java
+++ textile-api-base/src/main/java/net/textilemc/textile/api/util/BooleanFunction.java
@@ -17,21 +17,17 @@
 package net.textilemc.textile.api.util;
 
 /**
- * NBT type ID constants. Useful for filtering by value type in a few cases.
+ * Represents a function that accepts an boolean-valued argument and produces a result.
+ *
+ * <p>This is the {@code boolean}-consuming primitive specialization for {@link java.util.function.Function}.
  */
-public final class NbtType {
-	public static final int END = 0;
-	public static final int BYTE = 1;
-	public static final int SHORT = 2;
-	public static final int INT = 3;
-	public static final int LONG = 4;
-	public static final int FLOAT = 5;
-	public static final int DOUBLE = 6;
-	public static final int BYTE_ARRAY = 7;
-	public static final int STRING = 8;
-	public static final int LIST = 9;
-	public static final int COMPOUND = 10;
-	public static final int INT_ARRAY = 11;
-
-	private NbtType() { }
+@FunctionalInterface
+public interface BooleanFunction<R> {
+	/**
+	 * Applies this function to the given argument.
+	 *
+	 * @param value the function argument
+	 * @return the function result
+	 */
+	R apply(boolean value);
 }
--- textile-api-base/src/main/java/net/textilemc/textile/api/util/TriState.java
+++ textile-api-base/src/main/java/net/textilemc/textile/api/util/TriState.java
@@ -16,16 +16,223 @@
 
 package net.textilemc.textile.api.util;
 
-public enum TriState {
-	FALSE,
-	DEFAULT,
-	TRUE;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
-	public static TriState of(boolean b) {
-		return b ? TRUE : FALSE;
+import org.jetbrains.annotations.Nullable;
+
+public enum TriState {
+	/**
+	 * Represents the Boolean value of {@code false}.
+	 */
+	TRUE(true) {
+		@Override
+		public TriState and(TriState other) {
+			return other == TRUE || other == UNDEFINED ? TRUE : FALSE;
+		}
+
+		@Override
+		public TriState or(TriState other) {
+			return TRUE;
+		}
+
+		@Override
+		public TriState negate() {
+			return FALSE;
+		}
+	},
+	/**
+	 * Represents the Boolean value of {@code true}.
+	 */
+	FALSE(false) {
+		@Override
+		public TriState and(TriState other) {
+			return FALSE;
+		}
+
+		@Override
+		public TriState or(TriState other) {
+			return other == TRUE ? TRUE : FALSE;
+		}
+
+		@Override
+		public TriState negate() {
+			return TRUE;
+		}
+	},
+	/**
+	 * Represents a value that refers to a "default" value, often as a fallback.
+	 * Stores a Boolean value of {@code null}
+	 */
+	UNDEFINED(null) {
+		@Override
+		public TriState and(TriState other) {
+			return other;
+		}
+
+		@Override
+		public TriState or(TriState other) {
+			return other;
+		}
+
+		/**
+		 * @return itself, as it can not be negated
+		 */
+		@Override
+		public TriState negate() {
+			return this;
+		}
+	};
+
+	private final Boolean value;
+
+	TriState(Boolean value) {
+		this.value = value;
+	}
+
+	/**
+	 * Gets the corresponding tri-state from a boolean value.
+	 *
+	 * @param bool the boolean value
+	 * @return {@link TriState#TRUE} or {@link TriState#FALSE} depending on the value of the boolean.
+	 */
+	public static TriState of(boolean bool) {
+		return bool ? TRUE : FALSE;
+	}
+
+	/**
+	 * Gets a tri-state from a nullable boxed boolean.
+	 *
+	 * @param bool the boolean value
+	 * @return {@link TriState#UNDEFINED} if {@code null}.
+	 * Otherwise {@link TriState#TRUE} or {@link TriState#FALSE} depending on the value of the boolean.
+	 */
+	public static TriState of(@Nullable Boolean bool) {
+		return bool == null ? UNDEFINED : of(bool.booleanValue());
+	}
+
+	/**
+	 * Performs a {@code AND} operation on another tri-state.
+	 * @param other the tri-state that must operated with
+	 * @return the result of the operation
+	 */
+	public abstract TriState and(TriState other);
+
+	/**
+	 * Performs a {@code OR} operation on another tri-state.
+	 * @param other the tri-state that must operated with
+	 * @return the result of the operation
+	 */
+	public abstract TriState or(TriState other);
+
+	/**
+	 * Negates the value of the tri-state.
+	 * @return the negated value
+	 */
+	public abstract TriState negate();
+
+	/**
+	 * Flat maps the boolean value of this tri-state if it is {@link TriState#TRUE} or {@link TriState#FALSE}.
+	 *
+	 * @param mapper the mapper to use
+	 * @param <T> the type of object being supplier by the mapper
+	 * @return a stream containing the flattened values; {@link Stream#empty()} if the tri-state is {@link TriState#UNDEFINED} or the value provided by the mapper is {@code null}.
+	 */
+	public <T> Stream<T> flatMap(BooleanFunction<? extends Stream<T>> mapper) {
+		Objects.requireNonNull(mapper, "Mapper function cannot be null");
+
+		if (this == UNDEFINED) {
+			return Stream.empty();
+		}
+
+		return mapper.apply(this.get());
+	}
+
+	public Stream<Boolean> stream() {
+		if (this == UNDEFINED) {
+			return Stream.empty();
+		}
+
+		return Stream.of(this.get());
+	}
+
+	/**
+	 * Gets the value of this tri-state.
+	 * If the value is {@link TriState#UNDEFINED} then use the supplied value.
+	 *
+	 * @param value the value to fallback to
+	 * @return the value of the tri-state or the supplied value if {@link TriState#UNDEFINED}.
+	 */
+	public boolean orElse(boolean value) {
+		return this == UNDEFINED ? value : this.get();
+	}
+
+	public Optional<Boolean> getOptional() {
+		return this.value == null ? Optional.empty() : Optional.of(this.value);
+	}
+
+	/**
+	 * Gets the value of this tri-state.
+	 * If the value is {@link TriState#UNDEFINED} then use the supplied value.
+	 *
+	 * @param supplier the supplier used to get the value to fallback to
+	 * @return the value of the tri-state or the value of the supplier if the tri-state is {@link TriState#UNDEFINED}.
+	 */
+	public boolean orElseGet(BooleanSupplier supplier) {
+		return this == UNDEFINED ? supplier.getAsBoolean() : this.get();
 	}
 
 	public boolean get() {
 		return this == TRUE;
+	}
+
+	/**
+	 * Maps the boolean value of this tri-state if it is {@link TriState#TRUE} or {@link TriState#FALSE}.
+	 *
+	 * @param mapper the mapper to use
+	 * @param <T> the type of object being supplier by the mapper
+	 * @return an optional containing the mapped value; {@link Optional#empty()} if the tri-state is {@link TriState#UNDEFINED} or the value provided by the mapper is {@code null}.
+	 */
+	public <T> Optional<T> map(BooleanFunction<? extends T> mapper) {
+		Objects.requireNonNull(mapper, "Mapper function cannot be null");
+
+		if (this == UNDEFINED) {
+			return Optional.empty();
+		}
+
+		return Optional.ofNullable(mapper.apply(this.get()));
+	}
+
+	/**
+	 * Gets the value of this tri-state, or throws an exception if this tri-state's value is {@link TriState#UNDEFINED}.
+	 *
+	 * @param exceptionSupplier the supplying function that produces an exception to be thrown
+	 * @param <X> Type of the exception to be thrown
+	 * @return the value
+	 * @throws X if the value is {@link TriState#UNDEFINED}
+	 */
+	public <X extends Throwable> boolean orElseThrow(Supplier<X> exceptionSupplier) throws X {
+		if (this != UNDEFINED) {
+			return this.get();
+		}
+
+		throw exceptionSupplier.get();
+	}
+
+	/**
+	 * Gets the value of this tri-state, or throws a {@link NoSuchElementException} if this tri-state's value is {@link TriState#UNDEFINED}.
+	 *
+	 * @return the value
+	 */
+	public boolean orElseThrow() throws NoSuchElementException {
+		if (this == UNDEFINED) {
+			throw new NoSuchElementException();
+		}
+
+		return this.get();
 	}
 }
