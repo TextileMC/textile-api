a71cacc0a43e97e108963675e7a64d1e38eb5700
SD <harrypotter.starwars.shrish@gmail.com>
2021-01-06 14:32:20 +0530

    Start testmod work

---
+++ textile-recipe-api-v1/src/testmod/java/net/textilemc/textile/api/recipe/event/v1/test/RecipeTest.java
@@ -0,0 +1,11 @@
+package net.textilemc.textile.api.recipe.event.v1.test;
+
+import net.textilemc.textile.api.recipe.event.v1.ShapelessRecipe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RecipeTest implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
