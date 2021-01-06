8df0232a64bcb098b5b9b0ec0c504ffc80fd9136
SD <harrypotter.starwars.shrish@gmail.com>
2021-01-06 15:23:49 +0530

    Yeet some parts

--- textile-recipe-api-v1/src/testmod/java/net/textilemc/textile/api/recipe/event/v1/test/RecipeTest.java
+++ textile-recipe-api-v1/src/testmod/java/net/textilemc/textile/api/recipe/event/v1/test/RecipeTest.java
@@ -2,10 +2,21 @@ package net.textilemc.textile.api.recipe.event.v1.test;
 
 import net.textilemc.textile.api.recipe.event.v1.ShapelessRecipe;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
 import net.fabricmc.api.ModInitializer;
 
 public class RecipeTest implements ModInitializer {
 	@Override
 	public void onInitialize() {
+		ShapelessRecipe.DISPATCHER.accept(ShapelessRecipe.builder(new ItemStack(Item.COAL))
+				.add(new ItemStack(Block.LOG))
+				.add(new ItemStack(Block.DIRT))
+				.add(new ItemStack(Block.DIRT))
+				.add(new ItemStack(Block.DIRT))
+				.add(new ItemStack(Block.DIRT))
+				.build());
 	}
 }
